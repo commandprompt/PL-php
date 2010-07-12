9c34d5b665e2e42ae642b55657f7a993c9ed627a
Alvaro Herrera <alvherre@commandprompt.com>
2010-07-12 18:28:44 +0000

    Add install scripts to ease creation of pg_pltemplate entries.

---
+++ install82.sql
@@ -0,0 +1,5 @@
+INSERT INTO pg_pltemplate VALUES
+('plphp', 't', 'plphp_call_handler', 'plphp_validator', '$libdir/plphp', NULL);
+
+INSERT INTO pg_pltemplate VALUES
+('plphpu', 'f', 'plphp_call_handler', 'plphp_validator', '$libdir/plphp', NULL);
